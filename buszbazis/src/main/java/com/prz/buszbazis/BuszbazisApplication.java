468d00a6e6924802e356f06602acfb671cfab1e4
Pap Richard Zoltan <paprichardpap@gmail.com>
2024-12-17 21:06:41 +0100

    Poc

---
+++ buszbazis/src/main/java/com/prz/buszbazis/BuszbazisApplication.java
@@ -0,0 +1,13 @@
+package com.prz.buszbazis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuszbazisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuszbazisApplication.class, args);
+	}
+
+}
---
+++ buszbazis/src/test/java/com/prz/buszbazis/BuszbazisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prz.buszbazis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuszbazisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
